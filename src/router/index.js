bdec48f173dcecb58de19d22dccb2a24cc5cc3d6
Evgenii <shestakovevgeniy11@gmail.com>
2021-05-14 11:16:29 +0300

    hardcode

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Weather",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Weather.vue"),
   },
   {
     path: "/about",
@@ -18,15 +21,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/weather",
-    name: "Weather",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Weather.vue"),
   },
 ];
 
